f50cb87cf24bc179862f638a365863fcb40b4c0f
Kholofelo-kga <124498146+Kholofelo-kga@users.noreply.github.com>
2025-11-04 20:16:40 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,49 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+
+import App from "./App.jsx";
+import Welcome from "./pages/Welcome.jsx";
+import Register from "./pages/Register.jsx";
+import Report from "./pages/Report.jsx";
+import Admin from "./pages/Admin.jsx";
+import MyReports from "./pages/MyReports.jsx";
+import RequireAuth from "./components/RequireAuth.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/report"
+          element={
+            <RequireAuth>
+              <Report />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/my"
+          element={
+            <RequireAuth>
+              <MyReports />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            <RequireAuth>
+              <Admin />
+            </RequireAuth>
+          }
+        />
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
 
